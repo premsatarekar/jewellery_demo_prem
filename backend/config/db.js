43d00b407d02f7b2ea065a84ad9a0bc44a3bb4a2
premsatarekar <premsatarekar@gmail.com>
2025-06-26 23:13:16 +0530

    💎 added MySQL DB connection + .env config

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host:     process.env.DB_HOST || "localhost",
-  user:     process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "prem@#123",
-  database: process.env.DB_NAME || "jewellery_ms",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
